c81a583bdf9d1eed237718b5682a6ce1ea9d39fd
mzakyadinata <zakyadinata07@gmail.com>
2026-04-14 23:10:19 +0700

    fixing image

--- src/App.jsx
+++ src/App.jsx
@@ -14,28 +14,28 @@ export default function App() {
       title: "Presensia Website App",
       desc: "Aplikasi absensi berbasis web ",
       tech: ["React JS", "Tailwind CSS", "Daisy UI"],
-      image: "/src/assets/presensia-app.png",
+      image: "/presensia-app.png",
       links: "https://github.com/mzakyadinata/presensia-app.git",
     },
     {
       title: "Landing Page Presensia",
       desc: "Landing page produk Presensia",
       tech: ["React JS", "Tailwind CSS"],
-      image: "/src/assets/presensia-lp.png",
+      image: "/presensia-lp.png",
       links: "https://github.com/mzakyadinata/presensia-landingPage.git",
     },
     {
       title: "Website Internal MTS Roudhotul Banat",
       desc: "Website administrasi internal MTS Roudhotul Banat",
       tech: ["React JS", "Tailwind CSS", "Daisy UI"],
-      image: "/src/assets/samatsaruba.png",
+      image: "/samatsaruba.png",
       links: "https://github.com/IanFreez/samatsaruba.git",
     },
     {
       title: "Design Website Elmanuk",
       desc: "Design website jual kebutuhan burung",
       tech: ["Figma"],
-      image: "/src/assets/elmanuk.png",
+      image: "/elmanuk.png",
       links:
         "https://www.figma.com/design/UcPYUpGId6WV14TEaDgbQA/elmanuk?m=auto&t=kV6O5bw5iySgF6Ew-6",
     },
@@ -43,7 +43,7 @@ export default function App() {
       title: "Redesign Website Labschool Unesa",
       desc: "Redesign website milik Labschool Unesa ke design yang lebih fresh dan baru",
       tech: ["Figma"],
-      image: "/src/assets/redesign-web.png",
+      image: "/redesign-web.png",
       links:
         "https://www.figma.com/proto/aH4obxHoJFdcGVBya07XI4?node-id=0-1&t=kV6O5bw5iySgF6Ew-6",
     },
@@ -73,15 +73,15 @@ export default function App() {
       {/* Background */}
       <div className="absolute w-[500px] h-[500px] bg-purple-600/20 blur-[120px] top-[-100px] left-[-100px]"></div>
       {/* Hero Section */}
-      <section className="min-h-screen flex flex-col md:flex-row items-center justify-between px-8 md:px-20 pt-0">
+      <section className="flex flex-col items-center justify-between min-h-screen px-8 pt-0 md:flex-row md:px-20">
         <div data-aos="fade-up">
-          <h1 className="text-5xl md:text-7xl font-bold leading-tight">
+          <h1 className="text-5xl font-bold leading-tight md:text-7xl">
             Halo, saya <br />
-            <span className="bg-gradient-to-r from-purple-400 to-purple-600 bg-clip-text text-transparent">
+            <span className="text-transparent bg-gradient-to-r from-purple-400 to-purple-600 bg-clip-text">
               Zaky
             </span>
           </h1>
-          <p className="mt-6 text-gray-400 max-w-md">
+          <p className="max-w-md mt-6 text-gray-400">
             Saya seorang Frontend Web Developer. Saya memiliki pengalaman dalam
             membuat aplikasi web dan desain yang menarik.
           </p>
@@ -91,7 +91,7 @@ export default function App() {
                 .getElementById("projects")
                 .scrollIntoView({ behavior: "smooth" })
             }
-            className="mt-8 px-6 py-3 bg-purple-600 rounded-xl hover:scale-105 transition shadow-lg shadow-purple-600/30"
+            className="px-6 py-3 mt-8 transition bg-purple-600 shadow-lg rounded-xl hover:scale-105 shadow-purple-600/30"
           >
             Lihat Project
           </button>
@@ -99,23 +99,23 @@ export default function App() {
         <div className="relative mt-10 md:mt-0" data-aos="fade-left">
           {/* Glow  */}
           <div className="absolute inset-0 flex items-center justify-center">
-            <div className="w-102 h-102 rounded-full bg-purple-600/20 blur-3xl"></div>
+            <div className="rounded-full w-102 h-102 bg-purple-600/20 blur-3xl"></div>
           </div>
           {/* Ring  */}
           <div className="absolute inset-0 flex items-center justify-center">
-            <div className="w-102 h-102 rounded-full border border-purple-500/30"></div>
+            <div className="border rounded-full w-102 h-102 border-purple-500/30"></div>
           </div>
           {/* Photo */}
           <img
-            src="/src/assets/author.png"
+            src="/author.png"
             className="relative w-64 md:w-110 -mt-20 z-10 drop-shadow-[0_0_30px_rgba(168,85,247,0.6)]"
           />
         </div>
       </section>
       {/* Projects Section */}
-      <section id="projects" className="px-8 md:px-20 py-20">
-        <h2 className="text-3xl md:text-4xl font-bold mb-10">Project</h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <section id="projects" className="px-8 py-20 md:px-20">
+        <h2 className="mb-10 text-3xl font-bold md:text-4xl">Project</h2>
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
           {projects.map((project, index) => (
             <div
               key={index}
@@ -128,7 +128,7 @@ export default function App() {
               />
 
               {/* CONTENT */}
-              <div className="p-4 flex flex-col flex-grow">
+              <div className="flex flex-col flex-grow p-4">
                 {/* TITLE (dibatasi 2 baris) */}
                 <h3 className="text-lg font-semibold line-clamp-2 min-h-[56px]">
                   {project.title}
@@ -144,7 +144,7 @@ export default function App() {
                   {project.tech.map((item, i) => (
                     <span
                       key={i}
-                      className="text-xs px-3 py-1 bg-purple-600/20 text-purple-300 rounded-full"
+                      className="px-3 py-1 text-xs text-purple-300 rounded-full bg-purple-600/20"
                     >
                       {item}
                     </span>
@@ -167,17 +167,17 @@ export default function App() {
         </div>
       </section>
       {/* Experience Section */}
-      <section className="px-8 md:px-20 py-20">
-        <h2 className="text-3xl font-bold mb-10">Pengalaman</h2>
+      <section className="px-8 py-20 md:px-20">
+        <h2 className="mb-10 text-3xl font-bold">Pengalaman</h2>
         <div className="bg-[#111] p-6 rounded-2xl flex flex-col md:flex-row gap-6 hover:shadow-lg hover:shadow-purple-500/10 transition">
           <img
-            src="/src/assets/otakkanan-logo.png"
+            src="/otakkanan-logo.png"
             className="w-full md:w-1/2 rounded-xl"
           />
           <div>
             <h3 className="text-xl font-semibold">PT Otak Kanan</h3>
-            <p className="text-gray-400 text-sm">Feb 2025 - Jun 2025</p>
-            <ul className="mt-3 text-gray-400 text-sm flex flex-col gap-2">
+            <p className="text-sm text-gray-400">Feb 2025 - Jun 2025</p>
+            <ul className="flex flex-col gap-2 mt-3 text-sm text-gray-400">
               <li>
                 - Mengembangkan landing page produk Presensia menggunakan
                 ReactJS
@@ -199,15 +199,15 @@ export default function App() {
         </div>
       </section>
       {/* Skills Section */}
-      <section className="px-8 md:px-20 py-20">
-        <h2 className="text-3xl font-bold mb-10">Skills</h2>
-        <div className="grid md:grid-cols-3 gap-8">
+      <section className="px-8 py-20 md:px-20">
+        <h2 className="mb-10 text-3xl font-bold">Skills</h2>
+        <div className="grid gap-8 md:grid-cols-3">
           {skills.map((skill, i) => (
             <div
               key={i}
               className="bg-[#111] p-6 rounded-xl hover:scale-105 transition"
             >
-              <h3 className="text-purple-500 mb-4">{skill.title}</h3>
+              <h3 className="mb-4 text-purple-500">{skill.title}</h3>
 
               <div className="flex flex-col gap-3">
                 {skill.items.map((item, j) => (
@@ -225,28 +225,28 @@ export default function App() {
         </div>
       </section>
       {/* Contact Section */}
-      <section className="px-8 md:px-20 py-24">
+      <section className="px-8 py-24 md:px-20">
         <div className="bg-[#111] rounded-2xl p-10 text-center relative overflow-hidden">
           {/* glow */}
           <div className="absolute w-[300px] h-[300px] bg-purple-600/20 blur-[100px] top-[-50px] left-1/2 -translate-x-1/2"></div>
 
-          <h2 className="text-3xl font-bold mb-4 relative z-10">Kontak</h2>
+          <h2 className="relative z-10 mb-4 text-3xl font-bold">Kontak</h2>
 
-          <p className="text-gray-400 mb-10 relative z-10">
+          <p className="relative z-10 mb-10 text-gray-400">
             Tertarik bekerja sama atau ingin berdiskusi? Hubungi saya
           </p>
 
-          <div className="flex flex-col md:flex-row justify-center gap-6 relative z-10">
+          <div className="relative z-10 flex flex-col justify-center gap-6 md:flex-row">
             {/* EMAIL */}
             <div className="bg-[#0b0b0f] px-6 py-4 rounded-xl border border-white/10 hover:scale-105 transition">
               <p className="text-sm text-gray-400">Email</p>
-              <p className="text-white font-medium">zakyadinata07@gmail.com</p>
+              <p className="font-medium text-white">zakyadinata07@gmail.com</p>
             </div>
 
             {/* PHONE */}
             <div className="bg-[#0b0b0f] px-6 py-4 rounded-xl border border-white/10 hover:scale-105 transition">
               <p className="text-sm text-gray-400">No. Telepon</p>
-              <p className="text-white font-medium">0895410454398</p>
+              <p className="font-medium text-white">0895410454398</p>
             </div>
           </div>
         </div>
